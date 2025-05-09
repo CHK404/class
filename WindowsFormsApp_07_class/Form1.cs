ccd2a83c85324c0fb263ba462b37b9a9c661411d
CHK404 <azbxabcd4@gmail.com>
2025-05-09 13:43:58 +0900

    writing

--- WindowsFormsApp_07_class/Form1.cs
+++ WindowsFormsApp_07_class/Form1.cs
@@ -15,6 +15,29 @@ namespace WindowsFormsApp_07_class
         public Form1()
         {
             InitializeComponent();
+
+            //4-1. 기본 생성자
+            Car car1 = new Car();
+            car1.Speed = 20;
+            car1.Color = "은색";
+            car1.PrintInfo();
+
+            Console.WriteLine("=======================");
+
+            //4-2. 매개변수 있는 생성자
+            Car car2 = new Car("그랜저", 50, "검정", 2024);
+            car2.Acceleration(30);
+            car2.PrintInfo();
+            Console.WriteLine("=======================");
+
+            //6. 메서드 사용
+            car2.Brake(100);
+            Console.WriteLine("=======================");
+
+            //7. static 메서드 호출
+            Car.ShowTotalCars();
+            Console.WriteLine("=======================");
+
         }
     }
 }
